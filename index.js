989c07e74cc51e77364a4b162a15954ce34ade4d
Emon Ahmed <technicalboyemon@gmail.com>
2022-04-22 17:22:41 +0600

    Working 2

--- index.js
+++ index.js
@@ -4,11 +4,36 @@ const app = express();
 const ObjectId = require("mongodb").ObjectId;
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
-
-
+const { createServer } = require("http");
+const { Server, Socket } = require("socket.io");
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
+
+// Socket.io
+const http = createServer(app);
+const io = new Server(http, {
+  cors: {
+    origin: "*",
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  socket.on("joinRoom", (id) => {
+    socket.join(id);
+    console.log({ joinRoom: socket.adapter.rooms });
+  });
+
+  socket.on("outRoom", (id) => {
+    socket.leave(id);
+    console.log({ outRoom: socket.adapter.rooms });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(socket.id + " disconnected");
+  });
+});
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@elearninghub.bwmnt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
@@ -27,6 +52,134 @@ async function run() {
 
     // ===================== USER API ========================
 
+    app.post("/watch/comment", async (req, res) => {
+      try {
+        const { content, blog_id, blog_user_id, user } = req.body;
+        console.log(req.body);
+        const newComment = new Comments({
+          user: user._id,
+          content,
+          blog_id,
+          blog_user_id,
+        });
+
+        const data = {
+          ...newComment._doc,
+          user,
+          createdAt: new Date().toISOString(),
+        };
+
+        io.to(`${blog_id}`).emit("createComment", data);
+
+        // await newComment.save();
+        const result = await coursesCollection.insertOne(data);
+        return res.json(result);
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: err.message });
+      }
+    });
+
+    // PUT API - Filter By Email [USER]
+    app.get("/users/account/:email", async (req, res) => {
+      const email = req.params.email;
+      const filter = { email: email };
+      const result = await usersCollection.findOne(filter);
+      res.json(result);
+    });
+
+    app.get("/watch/comment/", async (req, res) => {
+      try {
+        const { id } = req.params;
+        const data = await coursesCollection.aggregate([
+          {
+            $facet: {
+              totalData: [
+                {
+                  $match: {
+                    blog_id: ObjectId(id),
+                    comment_root: { $exists: false },
+                    reply_user: { $exists: false },
+                  },
+                },
+                {
+                  $lookup: {
+                    from: "users",
+                    let: { email: "$user" },
+                    pipeline: [
+                      { $match: { $expr: { $eq: ["$_id", "$$email"] } } },
+                      { $project: { name: 1 } },
+                    ],
+                    as: "user",
+                  },
+                },
+                { $unwind: "$user" },
+                {
+                  $lookup: {
+                    from: "comments",
+                    let: { cm_id: "$replyCM" },
+                    pipeline: [
+                      { $match: { $expr: { $in: ["$_id", "$$cm_id"] } } },
+                      {
+                        $lookup: {
+                          from: "users",
+                          let: { user_id: "$user" },
+                          pipeline: [
+                            {
+                              $match: { $expr: { $eq: ["$_id", "$$user_id"] } },
+                            },
+                            { $project: { name: 1, avatar: 1 } },
+                          ],
+                          as: "user",
+                        },
+                      },
+                      { $unwind: "$user" },
+                      {
+                        $lookup: {
+                          from: "users",
+                          let: { user_id: "$reply_user" },
+                          pipeline: [
+                            {
+                              $match: { $expr: { $eq: ["$_id", "$$user_id"] } },
+                            },
+                            { $project: { name: 1, avatar: 1 } },
+                          ],
+                          as: "reply_user",
+                        },
+                      },
+                      { $unwind: "$reply_user" },
+                    ],
+                    as: "replyCM",
+                  },
+                },
+                { $sort: { createdAt: -1 } },
+              ],
+              totalCount: [
+                {
+                  $match: {
+                    blog_id: ObjectId(id),
+                    comment_root: { $exists: false },
+                    reply_user: { $exists: false },
+                  },
+                },
+                { $count: "count" },
+              ],
+            },
+          },
+          {
+            $project: {
+              count: { $arrayElemAt: ["$totalCount.count", 0] },
+              totalData: 1,
+            },
+          },
+        ]);
+
+        return res.json({ comments });
+      } catch (err) {
+        console.log(err);
+        return res.status(500).json({ msg: err.message });
+      }
+    });
     // POST API - For USER
     app.post("/users", async (req, res) => {
       const newUser = req.body;
@@ -65,6 +218,13 @@ async function run() {
     // Get Single API - Filter By Email [Instructor]
     app.get("/users/instructor", async (req, res) => {
       const cursor = usersCollection.find({ isInstructor: true });
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
+    // Get Single API - Filter By Email [Student]
+    app.get("/users/student", async (req, res) => {
+      const cursor = usersCollection.find({ isStudent: true });
       const result = await cursor.toArray();
       res.json(result);
     });
@@ -125,6 +285,19 @@ async function run() {
       res.json(users);
     });
 
+    // Get Single API - Filter By Email [USER]
+    app.get("/course/account", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      console.log(email);
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = coursesCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
     //DELETE API - Course
     app.delete("/courses/:id", async (req, res) => {
       const id = req.params.id;
@@ -149,6 +322,18 @@ async function run() {
       res.json(result);
     });
 
+    // Get Single API - Filter By Email [USER]
+    app.get("/order/account", async (req, res) => {
+      let query = {};
+      const email = req.query.email;
+      if (email) {
+        query = { email: email };
+      }
+      const cursor = orderCollection.find(query);
+      const result = await cursor.toArray();
+      res.json(result);
+    });
+
     // GET SINGLE API - For ORDER
     app.get("/order/:id", async (req, res) => {
       const id = req.params.id;
@@ -157,6 +342,13 @@ async function run() {
       res.json(order);
     });
 
+    //DELETE API - ORDeR
+    app.delete("/order/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await orderCollection.deleteOne(query);
+      res.json(result);
+    });
   } finally {
     // await client.close();
   }
@@ -167,6 +359,6 @@ app.get("/", (req, res) => {
   res.send("Hello eLearning Platform!");
 });
 
-app.listen(port, () => {
+http.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
